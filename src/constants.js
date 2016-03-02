0e32ce5b4a0cc9c01117cdd80c30eb8297db1572
LiangCY <liangcynju@qq.com>
2016-03-02 13:58:53 +0800

    完成登录功能

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+const URL_BASE = 'http://114.212.113.228:3060'
+
+const URL_LOGIN = URL_BASE + '/login'
+
+export {
+  URL_LOGIN
+}
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,20 +2,42 @@
     <form>
         <div class="form-group">
             <label for="username">用户名</label>
-            <input type="text" class="form-control" id="username" placeholder="用户名">
+            <input v-model="username" type="text" class="form-control" id="username" placeholder="用户名">
         </div>
         <div class="form-group">
             <label for="password">密码</label>
-            <input type="password" class="form-control" id="password" placeholder="密码">
+            <input v-model="password" type="password" class="form-control" id="password" placeholder="密码">
         </div>
-        <button type="submit" class="btn btn-primary">登录</button>
+        <button v-on:click="doLogin" class="btn btn-primary">登录</button>
     </form>
 </template>
 <script>
-    import {buttons} from 'vue-strap'
+    import notie from 'notie'
+    import {URL_LOGIN} from '../constants'
     export default {
-        components: {
-            buttons
+        data(){
+            return {
+                username: '',
+                password: ''
+            }
+        },
+        methods: {
+            doLogin(){
+                this.$http.post(URL_LOGIN, {
+                    username: this.username,
+                    password: this.password
+                }).then(function (response) {
+                    let data = response.data;
+                    if (data.error == 0) {
+                        console.log(data);
+                        this.$router.go('/')
+                    } else {
+                        notie.alert(3, data.message, 2)
+                    }
+                }, function () {
+                    notie.alert(3, '请求失败', 2)
+                })
+            }
         }
     }
 </script>
@@ -23,6 +45,7 @@
     html {
         height: 100%;
     }
+
     body {
         display: flex;
         align-items: center;
